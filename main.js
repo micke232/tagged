cf9997c5e5857fbdb86940dcae5bffae6c902f98
Jonatan.Johnsson <tehuberwurst@hotmail.com>
2015-10-01 14:25:15 +0200

    main.js

--- main.js
+++ main.js
@@ -12,18 +12,21 @@ reqAnimFrame(update);       //60 fps up in this
 /*======== PLAYERS ===========*/
 
 var player1 = {
-    x : 0,
-    y : 0,
-    width : 50,
-    height : 50,
-    color: "blue"
+    x : 100,
+    y : 100,
+    r : 50,
+    sAngle : 0,
+    eAngle : 2*Math.PI,
+    color: "blue",
+    hej : 75
 };
 
 var player2 = {
-    x: 100,
-    y: 100,
-    width: 50,
-    height: 50,
+    x : 200,
+    y : 200,
+    r : 50,
+    sAngle : 0,
+    eAngle : 2*Math.PI,
     color: "red"
 };
 
@@ -31,9 +34,23 @@ var player2 = {
 /* ====== CANVAS UPDATE FUNCTION ===== */
 
 function update(){          //Draws/animates the canvas
+
     context.clearRect(0, 0, 600, 600);
+    context.beginPath();
     context.fillStyle = player1.color;
-    context.fillRect(player1.x, player1.y, player1.width, player1.height);
+    context.arc(player1.x, player1.y, player1.r, player1.sAngle, player1.eAngle);
+    context.fill();
+    context.closePath();
+
+    context.beginPath();
+    context.arc(player2.x, player2.y, player2.r, player2.sAngle, player2.eAngle);
+    context.fillStyle = player2.color;
+    context.fill();
+    context.closePath();
+
+    if((player2.x - player1.x) > (player2.r + player1.r) && (player2.y - player1.y) > (player2.r + player1.r) ){
+        console.log("HEJSAN!");
+    }
 
     reqAnimFrame(update);
 }
