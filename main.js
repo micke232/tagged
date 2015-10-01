f88ca226b3a774794a0404104b67ff8e67f64929
Mikael Modig <modig@modig-UX301LAA>
2015-10-01 15:07:17 +0200

    fixed collision

--- main.js
+++ main.js
@@ -12,49 +12,55 @@ reqAnimFrame(update);       //60 fps up in this
 /*======== PLAYERS ===========*/
 
 var player1 = {
-
-    x : 100,
-    y : 100,
-    r : 50,
-    sAngle : 0,
-    eAngle : 75,
-    color: "blue",
-    picked: false
+	x: 100,
+	y: 100,
+	radius: 50,
+	sAngle: 0,
+	eAngle: 75,
+	color: "blue",
+	picked: false
 };
 
 var player2 = {
-    x: 300,
-    y: 300,
-    r : 50,
-    sAngle : 0,
-    eAngle : 75,
-    color: "red",
-    picked: false
+	x: 300,
+	y: 300,
+	radius: 50,
+	sAngle: 0,
+	eAngle: 75,
+	color: "red",
+	picked: false
 
 };
-
-
+var distance;
+var tagged = false;
 /* ====== CANVAS UPDATE FUNCTION ===== */
 
 function update(){          //Draws/animates the canvas
 
-    context.clearRect(0, 0, 600, 600);
-    context.beginPath();
-    context.fillStyle = player1.color;
-    context.arc(player1.x, player1.y, player1.r, player1.sAngle, player1.eAngle);
-    context.fill();
-    context.closePath();
+	context.clearRect(0, 0, 600, 600);
+	context.beginPath();
+	context.fillStyle = player1.color;
+	context.arc(player1.x, player1.y, player1.radius, player1.sAngle, player1.eAngle);
+	context.fill();
+	context.closePath();
 
-    context.beginPath();
-    context.arc(player2.x, player2.y, player2.r, player2.sAngle, player2.eAngle);
-    context.fillStyle = player2.color;
-    context.fill();
-    context.closePath();
+	context.beginPath();
+	context.arc(player2.x, player2.y, player2.radius, player2.sAngle, player2.eAngle);
+	context.fillStyle = player2.color;
+	context.fill();
+	context.closePath();
 
-    /*if((player2.x - player1.x) > (player2.r + player1.r) && (player2.y - player1.y) > (player2.r + player1.r) ){
-        console.log("HEJSAN!");
-    }*/
+	distance = Math.sqrt(
+		((player1.x - player2.x) * (player1.x - player2.x)) + ((player1.y - player2.y) * (player1.y - player2.y))
+	);
 
+	if (distance < player1.radius + player2.radius){
+		if (!tagged){
+			console.log("tagged!");
+			alert("TAGGED!");
+			tagged = true;
+		};
+	};
 
 	reqAnimFrame(update);
 }
@@ -65,19 +71,19 @@ window.addEventListener("keydown", function(e){         //controls
 
 	if(e.keyCode == 37){
 		//vänster
-		if (player1.x > 50) player1.x -= 5;
+		if (player1.x > 50) player1.x -= 10;
 	}
 	if(e.keyCode == 38){
 		//upp
-		if (player1.y > 50) player1.y -= 5;
+		if (player1.y > 50) player1.y -= 10;
 	}
 	if(e.keyCode == 39){
 		//höger
-		if (player1.x < 600) player1.x += 5;
+		if (player1.x < 600) player1.x += 10;
 	}
 	if(e.keyCode == 40){
 		//ner
-		if (player1.y < 600) player1.y += 5;
+		if (player1.y < 600) player1.y += 10;
 	}
 });
 
